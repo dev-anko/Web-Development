35bbfd8bf31cb08de8c0cf079856c18b96d3480e
anıl sezgin <anlsezgin3@gmail.com>
2023-12-12 00:27:26 +0300

    update Calculator Project

--- JavaScript/Advanced JavaScript and DOM Manipulation/Calculator Project/index.js
+++ JavaScript/Advanced JavaScript and DOM Manipulation/Calculator Project/index.js
@@ -1,28 +1,20 @@
-const display = document.querySelector('#display');
-const buttons = document.querySelectorAll('button');
+let space = document.querySelector(".spc-1");
+let buttons = document.querySelectorAll("button");
+
 buttons.forEach((item) => {
     item.onclick = function() {
         if (item.id == 'clear') {
-            display.innerText = '';
-        } else if (item.id == 'backspace') {
-            let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length - 1);
-        } else if (display.innerText != '' && item.id == 'equal') {
-            display.innerText = eval(display.innerText);
-        } else if (display.innerText == '' && item.id == 'equal') {
-            display.innerText = 'Empty!';
-            setTimeout(() => (display.innerText = ''), 2000);
+            space.innerText = '';
+        } 
+        else if (space.innerText != '' && item.id == 'esit') {
+            space.innerText = eval(space.innerText);
+        } else if (space.innerText == '' && item.id == 'esit') {
+            space.innerText = 'Empty!';
+            setTimeout(() => (space.innerText = ''), 2000);
         } else {
-            display.innerText += item.id;
+            space.innerText += item.textContent;
         }
     }
 })
-const themeToggleBtn = document.querySelector('.theme-toggler');
-const calculator = document.querySelector('.dark');
-const toggleIcon = document.querySelector('.toggler-icon');
-let isDark = true;
-themeToggleBtn.onclick = () => {
-    calculator.classList.toggle('dark');
-    themeToggleBtn.classList.toggle('active');
-    isDark = !isDark;
-}
+
+
